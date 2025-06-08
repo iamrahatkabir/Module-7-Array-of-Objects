3cebf4790f9c955cb1f33f9cbec9b0a46cdcf24f
Rahat Kabir Ifty <iftymyalice@Iftys-MacBook-Air.local>
2025-06-09 03:53:08 +0600

    sort in aschending and descending order in optimized way

--- sort_array_of_object_3.cpp
+++ sort_array_of_object_3.cpp
@@ -35,6 +35,9 @@ public:
 bool cmp(Student l, Student r) // Custom Comparison Function;
 {
     // Short in ascending order
+    
+    // if(l.marks <= r.marks) //If marks are same;
+
     // if(l.marks < r.marks)
     // {
     //     return true;
@@ -42,9 +45,47 @@ bool cmp(Student l, Student r) // Custom Comparison Function;
     // else{
     //     return false;
     // }
-    return l.marks < r.marks; // Shortcut way to Short in ascending order;
+    // return l.marks < r.marks; // Shortcut way to Short in ascending order;
 
     //return l.marks > r.marks; //Short in descending order;
+
+    
+    //If all marks are equal sort according to roll  
+    //sort in descending order;
+    // if(l.marks > r.marks)
+    // {
+    //     return true;
+    // }
+    // else if(l.marks < r.marks)
+    // {
+    //     return false;
+    // }
+    // else{
+        // if(l.roll < r.roll) //Small roll will come first;
+        // {
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+
+    //     return l.roll < r.roll; //Small roll will come first;
+    // }
+
+
+    //Optimized way to sort in descending marks order & if marks are equal sort with roll in ascending order(smallest roll will come first);
+    if(l.marks == r.marks)
+    {
+        //This will return true or false
+        return l.roll < r.roll; //Ascending order roll sorting when marks are equal;
+    }
+    else{
+        return l.marks > r.marks; //Descending order sort;
+    }
+
+    //sort using turnery operator
+    // return (l.marks == r.marks) ? l.roll < r.roll : l.marks > r.marks;
+
 }
 
 int main()
